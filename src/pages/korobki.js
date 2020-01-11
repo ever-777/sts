aeeb732a5b54b5a2b356c0743da9ad193afb2750
term <term>
2020-01-11 12:42:46 +0400

    Hide subcatalog Kkorobki added

---
+++ src/pages/korobki.js
@@ -0,0 +1,66 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default () => (
+    <Layout>
+        <SEO title="Металлоконструкции"/>
+        <h1>Металлоконструкции</h1>
+        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel" data-pause="false" data-interval="3000">
+            <ol className="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="5"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="6"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="7"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="8"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="9"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="10"></li>
+            </ol>
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="/images/mconstructions/mconstruction1.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction2.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction3.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction4.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction5.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction6.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction7.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction8.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction9.jpg" className="d-block w-100" alt="..."/>
+                </div>
+                <div className="carousel-item">
+                    <img src="/images/mconstructions/mconstruction10.jpg" className="d-block w-100" alt="..."/>
+                </div>
+            </div>
+            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"/>
+                <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="sr-only">Next</span>
+            </a>
+        </div>
+                </Layout>
+)
